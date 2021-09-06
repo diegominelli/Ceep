8cf47593d48eee0066e609c89477f849b1e86868
Diego Minelli <dodi@gft.com>
2021-09-06 12:29:23 -0300

    Adicionando item na lista

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const criarTarefa = (event) => {
   event.preventDefault();
 
+  const lista = document.querySelector("[data-list]");
   const input = document.querySelector("[data-form-input]");
   const valor = input.value;
 
-  const tarefa = document.querySelector("[data-task]");
-  const conteudo = `<div class="content">${valor}</div>`;
+  const tarefa = document.createElement("li");
+  tarefa.classList.add("task");
 
+  const conteudo = `<p class="content">${valor}</p>`;
   tarefa.innerHTML = conteudo;
 
+  lista.appendChild(tarefa);
   input.value = "";
 };
 
