a48bc844876ab8f032121cbf2aa4296bc62dbef5
Diego Minelli <dodi@gft.com>
2021-09-06 11:44:24 -0300

    Observando o comportamento do formulario

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const novaTarefa = document.querySelector("[data-form-button]");
-
-novaTarefa.addEventListener("click", () => {
+const criarTarefa = (event) => {
+  event.preventDefault();
   const input = document.querySelector("[data-form-input]");
   const valor = input.value;
+
   console.log(valor);
-});
+  input.value = "";
+};
+
+const novaTarefa = document.querySelector("[data-form-button]");
+
+novaTarefa.addEventListener("click", criarTarefa);
