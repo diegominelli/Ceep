47e581a8367676b0427699b8a96d0fc89e877871
Diego Minelli <dodi@gft.com>
2021-09-06 12:52:26 -0300

    Criando o botao concluir

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const criarTarefa = (event) => {
   const conteudo = `<p class="content">${valor}</p>`;
   tarefa.innerHTML = conteudo;
 
+  tarefa.appendChild(BotaoConclui());
   lista.appendChild(tarefa);
   input.value = "";
 };
@@ -18,3 +19,13 @@ const criarTarefa = (event) => {
 const novaTarefa = document.querySelector("[data-form-button]");
 
 novaTarefa.addEventListener("click", criarTarefa);
+
+const BotaoConclui = () => {
+  const botaoConclui = document.createElement("button");
+  botaoConclui.classList.add("check-button");
+  botaoConclui.innerText = "concluir";
+  botaoConclui.addEventListener("click", () => {
+    console.log("Fui clicado");
+  });
+  return botaoConclui;
+};
